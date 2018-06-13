2304d825ddab7ecc8f454ee73e0acc2693fed417
Md Nehal Amin <nehalamin93@gmail.com>
2018-06-14 04:35:47 +0530

    modified migration file

--- lib/generators/dj_split/templates/migration.rb
+++ lib/generators/dj_split/templates/migration.rb
@@ -1,9 +1,10 @@
 class AddJobGroupIdToDelayedJobs < ActiveRecord::Migration
   def self.up
-    add_column :job_group_id, :string, :limit => 20
+    add_column :delayed_jobs, :job_group_id, :string, :limit => 20
+    add_index :delayed_jobs, :job_group_id
   end
   
   def self.down
-    remove_column :job_group_id, :string
+    remove_column :delayed_jobs, :job_group_id, :string
   end
 end
