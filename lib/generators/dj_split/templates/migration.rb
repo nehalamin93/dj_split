9ff84067baa3e940d2ac78cdab29b957e13cabd7
Md Nehal Amin <nehalamin93@gmail.com>
2018-07-28 17:48:05 +0530

    fix bug of unmatched migration name with class name

--- lib/generators/dj_split/templates/migration.rb
+++ lib/generators/dj_split/templates/migration.rb
@@ -1,4 +1,4 @@
-class AddJobGroupIdToDelayedJobs < ActiveRecord::Migration
+class AddSplitGroupIdToDelayedJobs < ActiveRecord::Migration
   def self.up
     add_column :delayed_jobs, :split_group_id, :string, :limit => 20
     add_index :delayed_jobs, :split_group_id
